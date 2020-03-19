0a7784e2267e5900af7f2b8492edb44a4d9715d0
Darren <dmcp@home.com>
2020-03-19 12:54:19 +0000

    setting up basic end points. These will slighlty change, no need for 'create' appended to url. Just use the request to decide

--- API/todo/main/controllers.py
+++ API/todo/main/controllers.py
@@ -4,4 +4,27 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return 'Main'
+    return 'Success'
+
+#Create todo
+@main.route('/todo/create') #, methods=['POST'])
+def createTodo():
+    return 'Create todo'
+
+#Read all todos
+@main.route('/todo') #, methods=['GET'])
+def getAllTodos():
+    return 'Get todos'
+
+#Update todo
+@main.route('/todo/update')#, methods=['PUT'])
+def updateTodo():
+    return 'Update todo'
+
+#Delete todo
+@main.route('/todo/delete')#, methods=['DELETE'])
+def deleteTodo():
+    return 'Delete Todo'
+
+
+
